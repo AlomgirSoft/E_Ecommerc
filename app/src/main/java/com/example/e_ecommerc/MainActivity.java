6991e1a64e7101deb33e5b0bdf10c93785a14346
AlomgirHossain <md.alomgir.hossain3435@gmail.com>
2023-06-13 20:50:33 +0600

    Second Commit

--- app/src/main/java/com/example/e_ecommerc/MainActivity.java
+++ app/src/main/java/com/example/e_ecommerc/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.e_ecommerc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.example.e_ecommerc.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding mainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
     }
 }
